a5777eaee3eacf4b5f1ce196a800160afd74a0fa
Saksham Saini <33866095+sakshamsaini@users.noreply.github.com>
2025-08-23 15:15:00 +0530

    Create file.js

---
+++ js/file.js
@@ -0,0 +1,10 @@
+// js/media-map.js
+// Each entry now has { type, url } for proper handling in main.js
+
+const MEDIA_MAP = {
+  0: { type: "video", url: "https://www.w3schools.com/tags/mov_bbb.mp4" },
+  1: { type: "video", url: "https://www.w3schools.com/tags/mov_bbb.mp4" },
+  2: { type: "video", url: "https://www.w3schools.com/tags/mov_bbb.mp4" },
+  3: { type: "video", url: "https://www.w3schools.com/tags/mov_bbb.mp4" }
+
+};
