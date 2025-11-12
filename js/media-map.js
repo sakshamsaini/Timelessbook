6a33ed551c57d42c86629cf359685a4ca8540246
Saksham Saini <33866095+sakshamsaini@users.noreply.github.com>
2025-11-12 12:08:34 +0530

    Update media-map.js

--- js/media-map.js
+++ js/media-map.js
@@ -1,7 +1,7 @@
 // media_map.js
 
 const MEDIA_MAP = {
-  0: { type: "video", url: "https://d1ewlcau89ptzk.cloudfront.net/Phase2_FinalRecieved_Compressed/DhwaniRathod_Uppercase.mp4" },
+  0: { type: "video", url: "https://d1ewlcau89ptzk.cloudfront.net/Phase2_FinalRecieved_Compressed/Pg 16-17_ Riyhad Kundanmal, architect and polo player.mp4" },
  /* 1: { type: "video", url: "https://d2wgmlzg2z28oi.cloudfront.net/20_21_Police%20Station_AR%20Transition_002.mp4" },
   2: { type: "video", url: "https://d2wgmlzg2z28oi.cloudfront.net/30_31_Flora%20Fountain_AR%20Transition_001.mp4" },
   3: { type: "video", url: "https://d2wgmlzg2z28oi.cloudfront.net/36_Horniman_AR%20Transition_002.mp4" },
